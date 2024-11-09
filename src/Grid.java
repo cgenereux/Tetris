dd23651b0bf3a0048f939b855c1bebb1e9c0ee88
Curtis <curtisgenereux01@gmail.com>
2024-11-08 18:27:20 -0700

    implemented the grid and a displayGrid() method

--- src/Grid.java
+++ src/Grid.java
@@ -3,8 +3,25 @@ public class Grid {
     private final int height = 20;
     private final int width = 10;
 
-    private int[][] map;
+    private int[][] grid; // maybe map is a better name guys idrk
 
+    public Grid() {
+        grid = new int[height][width];
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                grid[y][x] = 0;
+            }
+        }
+    }
 
+    public void displayGrid() {
+        grid = new int[height][width];
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                System.out.println(grid[x][y] +  " ");
+            }
+            System.out.println("\n");
+        }
+    }
 
 }
