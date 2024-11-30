8c24bc9f4658b8fd21244f5ea2b22873dd33c6f7
Curtis <curtisgenereux01@gmail.com>
2024-11-30 11:32:11 -0700

    added more blocks

---
+++ src/Blocks/JBlock.java
@@ -0,0 +1,7 @@
+package Blocks;
+
+public class JBlock extends Block {
+    public JBlock() {
+        super(new int[][]{{1, 0}, {1, 1}, {1, 2}, {0, 2}}, 'J', "Purple");
+    }
+}
---
+++ src/Blocks/LBlock.java
@@ -0,0 +1,7 @@
+package Blocks;
+
+public class LBlock extends Block {
+    public LBlock() {
+        super(new int[][]{{0, 0}, {0, 1}, {0, 2}, {1, 2}}, 'L', "Orange");
+    }
+}
