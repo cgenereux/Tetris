f18c61c2cfdd642efd55b5c670e794db08d3186d
Curtis <curtisgenereux01@gmail.com>
2024-11-30 10:50:37 -0700

    added a UniqueID to block and a char TypeID for better usage ability

--- src/Blocks/Block.java
+++ src/Blocks/Block.java
@@ -3,12 +3,15 @@ package Blocks;
 public class Block {
 
     private final int[][] shape; // it's relative coordinates. so like: {{0, 0}, {1, 0}, etc}}
-    private final int id;
     private final String color;
+    private final int uniqueId;
+    private final char typeId;
+    private static int uniqueIdCounter = 1;
 
-    public Block(int[][] shape, int id, String color) {
+    public Block(int[][] shape, char typeId, String color) {
+        this.typeId = typeId;
+        this.uniqueId = uniqueIdCounter++;
         this.shape = shape;
-        this.id = id;
         this.color = color;
     }
 
@@ -16,8 +19,12 @@ public class Block {
         return shape;
     }
 
-    public int getId() {
-        return id;
+    public char getTypeID() {
+        return typeId;
+    }
+
+    public int getUniqueID() {
+        return uniqueId;
     }
 
     public String getColor() {
