7bb7b34bafc8bb45323ecd8ec514f51ea6b42770
Curtis <curtisgenereux01@gmail.com>
2024-11-30 10:51:27 -0700

    just added left, right, and down parameters

--- src/Controls.java
+++ src/Controls.java
@@ -12,9 +12,9 @@ public class Controls extends KeyAdapter{ //receive key inputs
     public void keyPressed(KeyEvent e) {
         switch (e.getKeyCode()) {
             case KeyEvent.VK_UP: game.rotateBlock();
-            case KeyEvent.VK_RIGHT: game.moveRight();
-            case KeyEvent.VK_LEFT: game.moveLeft();
-            case KeyEvent.VK_DOWN: game.moveDown();
+            case KeyEvent.VK_RIGHT: game.shiftBlock("right");
+            case KeyEvent.VK_LEFT: game.shiftBlock("left");
+            case KeyEvent.VK_DOWN: game.shiftBlock("down");
             case KeyEvent.VK_SPACE: game.dropBlock();
         }
     }
