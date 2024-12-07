90d703d746da7f9290104944298782cde771b25e
Curtis GENEREUX <curtis.genereux@ucalgary.ca>
2024-12-06 19:23:17 -0700

    slight code cleanup

--- src/GameSpeed.java
+++ src/GameSpeed.java
@@ -2,18 +2,22 @@ public class GameSpeed {
     private final int  baseSpeed; //base speed
     private final int scoreThreshold; //how much to increase speed
     private final double scalingFactor;
+    private final int maxSpeed;
     private int currentSpeed; //current speed
 
-    public GameSpeed(){
+    public GameSpeed() {
         this.baseSpeed = 500; //the starting speed -> lower = faster
         this.currentSpeed = baseSpeed;
-        this.scoreThreshold = 150; //every 150 points, increase speed
+        this.scoreThreshold = 150; // every 150 points, increase speed
         this.scalingFactor = 100;
+        this.maxSpeed = 100;
     }
 
+    // a logarithmic function for speed to make the game more playable
     public void updateSpeed(int score) {
         int thresholdsCrossed = score / scoreThreshold;
-        this.currentSpeed = (int) Math.max(baseSpeed - (scalingFactor * Math.log(thresholdsCrossed + 1)), 100);
+        // cap speed at maxSpeed
+        this.currentSpeed = (int) Math.max(baseSpeed - (scalingFactor * Math.log(thresholdsCrossed + 1)), maxSpeed);
     }
 
     public int getCurrentSpeed(){
